e1cf1f5ca6e7b568bebc992657bedd9505c0ab3b
zhe <leafleaving@gmail.com>
2015-07-23 02:08:46 +0100

    new start

--- pass/urls.py
+++ pass/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import patterns, include, url
-
+from mainpage import views
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pass.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-	
-    url(r'^admin/', include(admin.site.urls)),
-	url(r'^main/$','encryption.views.main'),
-	url(r'^code/$','encryption.views.code'),
-	url(r'^decode/$','encryption.views.decode'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^main/$','encryption.views.main'),
+                       url(r'^code/$','encryption.views.code'),
+                       url(r'^decode/$','encryption.views.decode'),
+                       url(r'^$',views.index, name='index'),
 )
